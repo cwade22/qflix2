24c789494dfaab990d22007d563518d43210703c
Carlie Wade <carlie.wade@student.csulb.edu>
2021-06-25 19:15:27 -0700

    home screen

---
+++ screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Image, FlatList } from 'react-native';
+
+
+import { Text, View } from '../../components/Themed';
+
+import styles from './styles';
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+    
+    </View>
+  );
+}
+
+export default HomeScreen;
